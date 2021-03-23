edcf138d6ad1f003435c558fe97893944d9e2c95
f-shaker <f.s.shaker@gmail.com>
2021-03-23 14:01:50 -0400

    neut 5.4.0 files

--- scripts/hadd_numu.sh
+++ scripts/hadd_numu.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
-FILES="/disk01/sklb2/t2kmc/t2kmc_14c/t2k_14a_root/nu-mode/numu_x_numu/root/*.root"
-TOTAL_NB=30
+#NEUT 5.4.0 files
+FILES="/disk02/sklb3/t2k-sk.mc.release/t2kmc_19b/root/fhc/numu_x_numu/t2ksk19b.fqv4r0.fhc.numu_x_numu.00*.root"
+#NEUT 5.3.2 files
+#FILES="/disk01/sklb2/t2kmc/t2kmc_14c/t2k_14a_root/nu-mode/numu_x_numu/root/*.root"
+TOTAL_NB=500
 STR=" "
 _i=0
 OUT_FILE="/disk02/usr6/fshaker/numu_kin.root"
