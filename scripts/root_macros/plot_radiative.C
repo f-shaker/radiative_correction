043a9b0461bab8a89ad363f40e94e44c1f7d79dc
f-shaker <f.s.shaker@gmail.com>
2021-06-08 22:45:51 -0400

    Implement FCFV and nu_mu selection cuts, add header file

--- scripts/root_macros/plot_radiative.C
+++ scripts/root_macros/plot_radiative.C
@@ -1,42 +1,22 @@
-#include <iostream>
-#include "TFile.h"
-#include "TTree.h"
-#include "TCanvas.h"
-#include "TH1D.h"
-#include "TLatex.h"
-#include "TLegend.h"
-#include "TRatioPlot.h"
-#include <fstream>
+#include "plot_radiative.h"
+  // Sub-event finder
+  int fqnse;
+  // Single-ring fits
+  float fq1rmom[100][7];
+  float fq1rnll[100][7];
+  float fq1rpos[100][7][3];
+  float fq1rdir[100][7][3];
+  // Other variables
+  int evclass;
+  unsigned short int nhitac;
 
-using namespace std;
-//Function declarations
-//int find_particle_idx(int* pdg_arr, int size, int particle_pdg);
-int find_particle_idx(UChar_t* ipv_arr, int size, UChar_t particle_ipv);
-//supoorting functions
-void format_hist1D(TH1* hist, std::string title, int col , int width, int sty);
-void plot_hist1D(TH1* hist, std::string filename, std::string title, int col , int width, int sty);
-void plot_superimposed_hist1D(TH1D* hist1, TH1D* hist2, std::string filename, std::string title, std::string draw_opt1, std::string draw_opt2, TLatex* tex = NULL);
-//void plot_ratio_hist1D(TH1D* hist1, TH1D* hist2, std::string filename, std::string title, std::string draw_opt1, std::string draw_opt2, TLatex* tex = NULL);
-void plot_ratio_hist1D(TH1* hist1, TH1* hist2, std::string filename, std::string x_axis_title, std::string y_up_axis_title, std::string y_down_axis_title);
-void print_perc(size_t ientry, size_t total_entries, int perc_step);
-
-
-//Global variable configuration
-std::string in_file = "/home/fshaker/t2k/radiative-correction/analysis/root_files/radiative.root";
-std::string in_file_init = "/home/fshaker/t2k/radiative-correction/analysis/root_files/radiative_init.root";
-std::string in_file_fin = "/home/fshaker/t2k/radiative-correction/analysis/root_files/radiative_fin.root";
-
-std::string plot_dir = "/home/fshaker/t2k/radiative-correction/analysis/root_files/plots/";
-// to protect against mempory corruption. The max numnu I found in the first few files was 11
-//atmpd-trunk/src/analysis/apfit_comp/SKDST_Base.h was set to 50
-// for this particle gun I generated only 2 events , mu- and photon
-const int MAX_NB_PARTICLES = 2;
-const int MAX_FQ_FITS = 22; //ToDo CHANGE THAT IN ANY NEW FILES!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-float MUON_MASS = 105.66; //MeV/c^2
-
+  int fq_mr_nring[100];
+  int fq_mr_nring_mu_fin[100];
+  int fq_mr_nring_mu_init[100];
 //==========================================================
 void plot_radiative(){
 
+  TH1::AddDirectory(kFALSE); 
   TFile *f=new TFile(in_file.c_str()); // opens the root file
   TTree *tr=(TTree*)f->Get("h1"); // creates the TTree object
 
@@ -57,20 +37,27 @@ void plot_radiative(){
   bool is_2ring = false;
   bool is_3_more_ring = false;
   int fq_mr_fits;
-  int fq_mr_nring[MAX_FQ_FITS];
-  int fq_mr_nring_mu_fin[MAX_FQ_FITS];
-  int fq_mr_nring_mu_init[MAX_FQ_FITS];
+
   //fqmrpid   : fqmrpid[fqnmrfit][6]
   int fq_mr_pid[MAX_FQ_FITS][6];
-  
+
+
   float cos_theta;
+  //momentum binning
+  double mom_bining_arr[22];//22 entries for bin edges
+  for(int i = 0 ; i < 21; i++){
+    double eq_step = 50;
+    mom_bining_arr[i] = i*eq_step;    
+  }
+  mom_bining_arr[21] = 2000;
+
   //gamma histograms
-  TH1D * gamma_mom_all_hist = new TH1D("gamma_mom_all", "gamma_mom_all", 100, 0, 2000);
-  TH1D * gamma_mom_1ring_hist = new TH1D("gamma_mom_1ring", "gamma_mom_1ring", 100, 0, 2000);
-  TH1D * gamma_mom_2ring_hist = new TH1D("gamma_mom_2ring", "gamma_mom_2ring", 100, 0, 2000);
-  TH1D * gamma_mom_3morering_hist = new TH1D("gamma_mom_3morering", "gamma_mom_3morering", 100, 0, 2000);    
-  TH1D * gamma_mom_1muring_hist = new TH1D("gamma_mom_1muring", "gamma_mom_1muring", 100, 0, 2000);
-  TH1D * gamma_mom_1muring_1ering_hist = new TH1D("gamma_mom_1mu1e", "gamma_mom_1mu1e", 100, 0, 2000);
+  TH1D * gamma_mom_all_hist = new TH1D("gamma_mom_all", "gamma_mom_all", 21,  mom_bining_arr);
+  TH1D * gamma_mom_1ring_hist = new TH1D("gamma_mom_1ring", "gamma_mom_1ring", 21,  mom_bining_arr);
+  TH1D * gamma_mom_2ring_hist = new TH1D("gamma_mom_2ring", "gamma_mom_2ring", 21,  mom_bining_arr);
+  TH1D * gamma_mom_3morering_hist = new TH1D("gamma_mom_3morering", "gamma_mom_3morering", 21,  mom_bining_arr);    
+  TH1D * gamma_mom_1muring_hist = new TH1D("gamma_mom_1muring", "gamma_mom_1muring", 21,  mom_bining_arr);
+  TH1D * gamma_mom_1muring_1ering_hist = new TH1D("gamma_mom_1mu1e", "gamma_mom_1mu1e", 21,  mom_bining_arr);
 
   TH1D * cos_theta_all_hist = new TH1D("cos_theta_all", "cos_theta_all", 20, -1, 1);  
   TH1D * cos_theta_1r_hist = new TH1D("cos_theta_1ring", "cos_theta_1r", 20, -1, 1);  
@@ -80,12 +67,12 @@ void plot_radiative(){
   TH1D * cos_theta_1mu1epir_hist = new TH1D("cos_theta_1muepiring", "cos_theta_1muepir", 20, -1, 1);  
   
   //muon histograms
-  TH1D * mu_mom_all_hist = new TH1D("mu_mom_all", "mu_mom_all", 100, 0, 2000);
-  TH1D * mu_mom_1ring_hist = new TH1D("mu_mom_1ring", "mu_mom_1ring", 100, 0, 2000);
-  TH1D * mu_mom_2ring_hist = new TH1D("mu_mom_2ring", "mu_mom_2ring", 100, 0, 2000);
-  TH1D * mu_mom_3morering_hist = new TH1D("mu_mom_3morering", "mu_mom_3morering", 100, 0, 2000);    
-  TH1D * mu_mom_1muring_hist = new TH1D("mu_mom_1muring", "mu_mom_1muring", 100, 0, 2000);
-  TH1D * mu_mom_1muring_1ering_hist = new TH1D("mu_mom_1mu1e", "mu_mom_1mu1e", 100, 0, 2000);
+  TH1D * mu_mom_all_hist = new TH1D("mu_mom_all", "mu_mom_all", 21,  mom_bining_arr);
+  TH1D * mu_mom_1ring_hist = new TH1D("mu_mom_1ring", "mu_mom_1ring", 21,  mom_bining_arr);
+  TH1D * mu_mom_2ring_hist = new TH1D("mu_mom_2ring", "mu_mom_2ring", 21,  mom_bining_arr);
+  TH1D * mu_mom_3morering_hist = new TH1D("mu_mom_3morering", "mu_mom_3morering", 21,  mom_bining_arr);    
+  TH1D * mu_mom_1muring_hist = new TH1D("mu_mom_1muring", "mu_mom_1muring", 21,  mom_bining_arr);
+  TH1D * mu_mom_1muring_1ering_hist = new TH1D("mu_mom_1mu1e", "mu_mom_1mu1e", 21,  mom_bining_arr);
 
   //nb rings histograms
   TH1I * nring_mu_gamma_hist = new TH1I("nring_mu_gamma", "nring_mu_gamma", 6, 0, 6);
@@ -113,6 +100,12 @@ void plot_radiative(){
   tr->SetBranchAddress("fqnmrfit", &fq_mr_fits);
   tr->SetBranchAddress("fqmrnring", fq_mr_nring);
   tr->SetBranchAddress("fqmrpid", fq_mr_pid);   
+  //CCQE
+  tr->SetBranchAddress("nhitac", &nhitac);
+  //tr->SetBranchAddress("evclass", &evclass); not available for MC use nhitac and fv cuts, evclass takes daq status into consideration
+  tr->SetBranchAddress("fqnse", &fqnse);
+  tr->SetBranchAddress("fq1rpos", fq1rpos);
+  tr->SetBranchAddress("fq1rdir", fq1rdir);
 
 
   for (int i=0;i<tr->GetEntries();i++){
@@ -121,6 +114,9 @@ void plot_radiative(){
     print_perc(i, tr->GetEntries(), 10);
     // loop over the tree
     tr->GetEntry(i);
+    //Selection Cuts
+    if (is_FCFV(0, MUON, nhitac) == false) continue;
+
     // in GEANT particle code 1 = gamma, 6 = mu-
     int gamma_idx = find_particle_idx(particle_ipv_code, nb_particles, 1);
     int mu_idx = find_particle_idx(particle_ipv_code, nb_particles, 6);
@@ -408,3 +404,127 @@ void plot_ratio_hist1D(TH1* hist1, TH1* hist2, std::string filename, std::string
   delete canv;
 
 }
+
+//Code taken from CCQE Selection (CRIS)
+//fsamir: this function compute the minimum distance to a wall in either the radius of the z direction of the inner tank
+//if the distance is -ve, i.e outside the inner tank
+//It only takes the fitted 1 ring vertex postion and does not care about the direction of the particle.
+
+  // Lifted from minituple code
+  float ComputeWall(int nsubevent, fq_particle i_particle)
+  {
+    float x = fq1rpos[nsubevent][i_particle][0];
+    float y = fq1rpos[nsubevent][i_particle][1];
+    float z = fq1rpos[nsubevent][i_particle][2];
+
+
+    float Rmax = 1690.;
+    float Zmax = 1810.;
+    float rr   = sqrt(x*x + y*y);
+    float absz = TMath::Abs(z);
+    //check if vertex is outside tank
+    float signflg = 1.;
+    if (absz>Zmax) signflg = -1.;
+    if (rr>Rmax)   signflg = -1.;
+    //find min distance to wall
+    float distz = TMath::Abs(Zmax-absz);
+    float distr = TMath::Abs(Rmax-rr);
+    float dwall = signflg*fmin(distz,distr);
+    return dwall;
+  }
+
+   // FV Variables
+  // Lifted from Minituple code
+
+  float ComputeTowall(int nsubevent, fq_particle i_particle)
+  {
+    float x = fq1rpos[nsubevent][i_particle][0];
+    float y = fq1rpos[nsubevent][i_particle][1];
+    float z = fq1rpos[nsubevent][i_particle][2];
+
+    float dx = fq1rdir[nsubevent][i_particle][0];
+    float dy = fq1rdir[nsubevent][i_particle][1];
+    float dz = fq1rdir[nsubevent][i_particle][2];
+    
+    Double_t const R(1690);
+    Double_t l_b(100000.0), H;
+    Double_t l_t(100000.0);
+    Double_t A, B, C, RAD;
+    if(dx!=0 || dy!=0){
+      A = (dx*dx+dy*dy);
+      B = 2*(x*dx+y*dy);
+      C = (x*x+y*y-R*R);
+      RAD = (B*B) - (4*A*C);
+      l_b = ((-1*B) + sqrt(RAD))/(2*A);
+    }
+    if (dz==0){return l_b;}
+    else if(dz>0){H=1810;}
+    else if(dz<0){H=-1810;}
+    l_t=(H - z)/dz;
+    return  (l_t > l_b ? l_b:l_t);
+  }
+
+//Selection Cuts
+/*
+1.Fully-contained in SK fiducial volume: classified by OD activity and total PMT hits as
+fully contained events; wall > 50cm, towall > 250cm. Here “wall”is the distance between
+vertex and the nearest ID wall; “towall”is the distance between the vertex and ID wall
+along the direction at which the particle (in the case of multiple rings, it refers to the
+particle with the most energetic ring) travels.
+*/
+bool is_FCFV(int nsubevent, fq_particle i_particle, unsigned int nhitac){
+  if(nhitac >= 16) return false;
+  float wall_dist = ComputeWall(nsubevent, i_particle);
+  if(wall_dist <= 50 ) return false;
+  float to_wall_dist = ComputeTowall(nsubevent, i_particle);
+  if(to_wall_dist <= 250) return false;
+  return true;
+}
+/*
+2. Number of rings found by the fiTQun multi-ring fitter is one
+*/
+bool is_1ring(){
+  return (fq_mr_nring[0] == 1);
+}
+/*
+3.The ring is identified as muon-like by the single-ring fitter: ln (L_e /L_mu ) < 0.2 × p_e , where
+ln L_e is the fiTQun single-ring e-like hypothesis log likelihood, ln L_mu single-ring mu-like log
+likelihood, and p_e reconstructed electron momentum of single-ring e-like hypothesis
+*/
+bool pass_e_mu_nll_cut(){
+  bool is_mu = false;
+  float discr = fq1rnll[0][MUON]-fq1rnll[0][ELECTRON]-0.2*fq1rmom[0][ELECTRON];
+  if(discr < 0){
+    is_mu=true;
+  } 
+  return is_mu;
+}
+/*
+4.Reconstructed muon momentum of the single-ring mu-like hypothesis p_mu is larger than 200
+MeV/c
+*/
+bool pass_mu_mom_cut(float min_mu_mom){
+  return (fq1rmom[0][MUON] > min_mu_mom);
+}
+/*
+5. Number of sub-events (identified by hits timing clusters) is 1 or 2 (i.e. number of decay
+electrons is 0 or 1).
+*/
+bool pass_nb_decay_e_cut(){
+  return ( (fqnse == 1) || (fqnse ==2) );
+}
+/*
+6.fiTQun pi+ rejection cut: ln (L_pi+ /L_mu ) < 0.15 × p_mu , where ln L_pi+ is the log likelihood of
+fiTQun single-ring pi+ hypothesis
+*/
+bool pass_pi_mu_nll_cut(){
+  bool is_mu = false;
+  float discr = fq1rnll[0][MUON]-fq1rnll[0][PION]-0.15*fq1rmom[0][MUON];
+  if(discr < 0){
+    is_mu=true;
+  } 
+  return is_mu;  
+}
+
+
+
---
+++ scripts/root_macros/plot_radiative.h
@@ -0,0 +1,75 @@
+#include <iostream>
+#include "TFile.h"
+#include "TTree.h"
+#include "TCanvas.h"
+#include "TH1D.h"
+#include "TLatex.h"
+#include "TLegend.h"
+#include "TRatioPlot.h"
+#include <fstream>
+
+using namespace std;
+//Types Definition
+// Particle indices for SK arrays
+typedef enum{
+    ELECTRON = 1,
+    MUON,
+    PION
+  } fq_particle;
+//global variables
+ // FiTQun variables
+  // Sub-event finder
+  extern int fqnse;
+  // Single-ring fits
+  extern float fq1rmom[100][7];
+  extern float fq1rnll[100][7];
+  extern float fq1rpos[100][7][3];
+  extern float fq1rdir[100][7][3];
+
+  // pi0 fit
+  extern float fqpi0nll[2];
+  extern float fqpi0mass[2];
+
+  // Multi=ring fit
+  extern int fqmrnring[100];
+  extern  int fq_mr_nring[100];  
+  // Other variables
+  extern int evclass;
+  extern unsigned short int nhitac;
+//Function declarations
+//int find_particle_idx(int* pdg_arr, int size, int particle_pdg);
+int find_particle_idx(UChar_t* ipv_arr, int size, UChar_t particle_ipv);
+//supoorting functions
+void format_hist1D(TH1* hist, std::string title, int col , int width, int sty);
+void plot_hist1D(TH1* hist, std::string filename, std::string title, int col , int width, int sty);
+void plot_superimposed_hist1D(TH1D* hist1, TH1D* hist2, std::string filename, std::string title, std::string draw_opt1, std::string draw_opt2, TLatex* tex = NULL);
+//void plot_ratio_hist1D(TH1D* hist1, TH1D* hist2, std::string filename, std::string title, std::string draw_opt1, std::string draw_opt2, TLatex* tex = NULL);
+void plot_ratio_hist1D(TH1* hist1, TH1* hist2, std::string filename, std::string x_axis_title, std::string y_up_axis_title, std::string y_down_axis_title);
+void print_perc(size_t ientry, size_t total_entries, int perc_step);
+
+//code from CCQE Selection start:
+float ComputeWall(int nsubevent, fq_particle i_particle);
+float ComputeTowall(int nsubevent, fq_particle i_particle);
+//end
+
+bool is_FCFV(int nsubevent, fq_particle i_particle, unsigned int nhitac);
+bool is_1ring();
+bool pass_e_mu_nll_cut();
+bool pass_pi_mu_nll_cut();
+bool pass_nb_decay_e_cut();
+bool pass_mu_mom_cut(float min_mu_mom =200);
+
+//Global variable configuration
+std::string in_file = "/home/fshaker/t2k/radiative-correction/analysis/root_files/radiative.root";
+std::string in_file_init = "/home/fshaker/t2k/radiative-correction/analysis/root_files/radiative_init.root";
+std::string in_file_fin = "/home/fshaker/t2k/radiative-correction/analysis/root_files/radiative_fin.root";
+
+std::string plot_dir = "/home/fshaker/t2k/radiative-correction/analysis/root_files/plots/";
+// to protect against mempory corruption. The max numnu I found in the first few files was 11
+//atmpd-trunk/src/analysis/apfit_comp/SKDST_Base.h was set to 50
+// for this particle gun I generated only 2 events , mu- and photon
+const int MAX_NB_PARTICLES = 2;
+const int MAX_FQ_FITS = 22; //ToDo CHANGE THAT IN ANY NEW FILES!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+float MUON_MASS = 105.66; //MeV/c^2
+
+
