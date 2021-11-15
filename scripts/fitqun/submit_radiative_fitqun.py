5714d15e1d082ef0066ae3981477e5edb6df4fac
f-shaker <f.s.shaker@gmail.com>
2021-11-15 18:51:15 -0500

    minor modification for submission scripts (add a configuration section)

--- scripts/fitqun/submit_radiative_fitqun.py
+++ scripts/fitqun/submit_radiative_fitqun.py
@@ -1,15 +1,31 @@
 import os
 import time
-#number of files = the output of the splitting process
-nb_files = 10
+#-------------------------------------------------------------------------------
+# Configuration
+#-------------------------------------------------------------------------------
+#input
+file_start = 0
+file_stop = 99 #will include this file in the submission
+file_step = 1
+sk_outdir = "../skdetsim/skdetsim_output/"
+sk_file_prefix = "skdetsim_out_"
+#output
+fitqun_outdir = "./fitqun_output/"
+fitqun_file_prefix = "fitqun_out_"
+# submission parameters
+submit_script_prefix = "submit_fitqun_"
+dump_prefix = "fitqun_dump_"
+# submission parameters (Do not change unless you know what you are doing!)
+parameter_fitqun_file = "./fiTQun.parameters.dat"
 queue = 'ALL'
 send_job = 'qsub -q {} -eo -lm 3gb -o {} {}'
+#-------------------------------------------------------------------------------
 
 def create_submission_script(file_idx):
-    submission_file_fitqun = "submit_fitqun_" + str(file_idx) + ".sh"
-    input_skdetsim_file = "../skdetsim/skdetsim_output/skdetsim_out_" + str(file_idx) + ".zbs"
-    output_fitqun_file = "./fitqun_output/fitqun_out_" + str(file_idx) + ".zbs"
-    parameter_fitqun_file = "./fiTQun.parameters.dat"
+    submission_file_fitqun = submit_script_prefix + str(file_idx) + ".sh"
+    input_skdetsim_file = sk_outdir + sk_file_prefix + str(file_idx) + ".zbs"
+    output_fitqun_file = fitqun_outdir + fitqun_file_prefix + str(file_idx) + ".zbs"
+    
     with open(submission_file_fitqun, 'w') as f:
         f.write("#!/bin/bash\n\n")
         f.write("source $HOME/setup_radiative_env.sh\n")
@@ -17,9 +33,8 @@ def create_submission_script(file_idx):
     return submission_file_fitqun
 
 
-
-for i in range(nb_files):
-    dump = "fitqun_dump_" + str(i) + ".txt"
+for i in range(file_start, file_stop+1, file_step):
+    dump = dump_prefix + str(i) + ".txt"
     sub_script= create_submission_script(i)
     sub_cmd= send_job.format(queue, dump, sub_script)
     os.system(sub_cmd)
--- scripts/skdetsim/submit_radiative_skdetsim.py
+++ scripts/skdetsim/submit_radiative_skdetsim.py
@@ -1,17 +1,29 @@
 import os
 import time
-#number of files = the output of the splitting process
+#-------------------------------------------------------------------------------
+# Configuration
+#-------------------------------------------------------------------------------
+#input
 file_start = 0
 file_stop = 99 #will include this file in the submission
 file_step = 1
-
+pg_dir = "../pg_files/"
+pg_file_prefix = "pg_mu_ID"
+#output
+sk_outdir = "./skdetsim_output/"
+sk_file_prefix = "sk_out_"
+# submission parameters
+submit_script_prefix = "submit_sk_"
+dump_prefix = "sk_dump_"
+# submission parameters (Do not change unless you know what you are doing!)
 queue = 'ALL'
 send_job = 'qsub -q {} -eo -lm 3gb -o {} {}'
+#-------------------------------------------------------------------------------
 
 def create_submission_script(file_idx):
-    submission_file_skdetsim = "submit_skdetsim_" + str(file_idx) + ".sh"
-    out_skdetsim = "./skdetsim_output/skdetsim_out_" + str(file_idx) + ".zbs"
-    input_pg_file = "../pg_files/pg_mu_ID_" + str(file_idx) + ".txt"
+    submission_file_skdetsim = submit_script_prefix + str(file_idx) + ".sh"
+    input_pg_file = pg_dir + pg_file_prefix + str(file_idx) + ".txt"
+    out_skdetsim = sk_outdir + sk_file_prefix + str(file_idx) + ".zbs"
     with open(submission_file_skdetsim, 'w') as f:
         f.write("#!/bin/bash\n\n")
         f.write("source $HOME/setup_radiative_env.sh\n")
@@ -21,7 +33,7 @@ def create_submission_script(file_idx):
 
 
 for i in range(file_start, file_stop+1, file_step):
-    dump = "skdetsim_dump_" + str(i) + ".txt"
+    dump = dump_prefix + str(i) + ".txt"
     sub_script= create_submission_script(i)
     sub_cmd= send_job.format(queue, dump, sub_script)
     os.system(sub_cmd)
